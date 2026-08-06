c2369747d55b1b3a2d4e89e801887a61dc5d43d2
Vasanthi <vasanthibojjagani21@gmail.com>
2026-08-06 11:40:21 +0530

    best-practices: enforce exact 131/48 aspect ratio and objectFit contain on Logo.jsx

--- src/modules/components/logo/Logo.jsx
+++ src/modules/components/logo/Logo.jsx
@@ -5,9 +5,9 @@ import { AppRoutes } from '../../../utils/consts/routes';
 
 const Logo = props => {
   return (
-    <Box  {...props}>
-      <Link href={AppRoutes.HOME}>
-          <Box width="100%" height="100%"  component='img' className='child' src={LOGO} alt="logo" />
+    <Box {...props}>
+      <Link href={AppRoutes.HOME} aria-label="UDEN Home Page">
+          <Box width="auto" height="100%" component='img' className='child' src={LOGO} alt="UDEN Official Platform Logo" sx={{ objectFit: 'contain', aspectRatio: '131/48' }} />
       </Link>
     </Box>
   );
